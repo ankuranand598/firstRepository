f99677a7c1bc65b8f4e1a414445448a6b046c548
Ankur598 <ankursingh.as278@gmail.com>
2021-04-28 08:30:55 +0530

    new edit of html

--- inputAdd.js
+++ inputAdd.js
@@ -1,4 +1,6 @@
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
-const sum = num1 + num2;
-console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+var person = prompt("Please enter your name", "Harry Potter");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";
+}
