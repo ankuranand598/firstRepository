b3ced57c3dc16aae7f1aa0d9c2d6bd325f94f2c7
Ankur598 <ankursingh.as278@gmail.com>
2021-05-03 09:49:40 +0530

    concate

--- day_7/concatenation.js
+++ day_7/concatenation.js
@@ -5,4 +5,4 @@ var d = "Purnea, Bihar";
 var e = "Purnea";
 var f = "Bihar";
 var g = 854301;
-console.log("My name is " + a + b + ". I am " + c + " years old. My address is " + d + "," + e +"," + f + "," +c+ g);
+console.log("My name is " + a + b + ". I am " + c + " years old. My address is " + d + "," + e +"," + f + "," +g);
